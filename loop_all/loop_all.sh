faa30765741f0cc45493f24cc3126dce0d577655
caseyanderson <cta@caseyanderson.com>
2018-09-27 14:56:04 -0700

    this is back now

--- loop_all/loop_all.sh
+++ loop_all/loop_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-THE_SCRIPT="/home/pi/SC3-RPiZeroW/fixed_media/loop_all/loop_all.scd"
-THE_DIR="/home/pi/uSAMPLES"
+THE_SCRIPT="/home/pi/slices_wkshp/loop_all/loop_all.scd"
+THE_DIR="/home/pi/recordings"
 
 ./sclang -a -l sclang.yaml $THE_SCRIPT $THE_DIR
